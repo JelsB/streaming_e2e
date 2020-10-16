b5d3f52d51c0c2a7c1b896246d46864b1cfd5f97
JelsB <boulangier.jels@gmail.com>
2020-10-16 10:57:19 +0200

    Add querries to populate input tables

--- streaming_e2e_input.session.sql
+++ streaming_e2e_input.session.sql
@@ -14,3 +14,19 @@ CREATE TABLE klant_provincie (
     verhuis_date date NOT NULL DEFAULT CURRENT_DATE,
     CONSTRAINT bill_product_pkey PRIMARY KEY (klant_id, provincie_id) -- explicit pk
 );
+INSERT INTO klant(klant_id, naam, email)
+values (1, 'Jan', 'jan@mail.com'),
+    (2, 'Kev', 'kev@mail.com'),
+    (3, 'Gman', 'gman@mail.com'),
+    (4, 'Gloop', 'gloop@mail.com');
+INSERT INTO provincie(provincie_id, naam)
+values (1, 'Vlaams-Brabant'),
+    (2, 'Limburg'),
+    (3, 'Antwerpen'),
+    (4, 'Waals-Branbant');
+INSERT into klant_provincie(klant_id, provincie_id, verhuis_date)
+VALUES (1, 1, '1996-12-02'),
+    (1, 3, '2020-08-02'),
+    (2, 1, '2000-02-01'),
+    (3, 4, '2020-10-16'),
+    (4, 2, '1990-10-20');
